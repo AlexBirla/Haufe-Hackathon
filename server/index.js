9d1f7dced3cd67e45219dc19d6f57ac29f5f9aa9
unknown <alexbirla32@gmail.com>
2022-10-01 12:47:21 +0300

    Fixed crashing issues for server

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const axios = require('axios')
 const { response } = require('express')
 const app = express()
 const port = 5000
-
+let collectors=[]
 app.get('/', async (req, res) => {
-  await axios.get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab').then((res) =>{
-    collectors=res.data.result.records
+  await axios.get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab&limit=5').then((response) =>{
+    collectors=response.data.result.records
+    res.send(collectors)
 })
-res.send(collectors)
+.catch((error) => {
+    res.json({result: "please try again"})
 })
+    
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
