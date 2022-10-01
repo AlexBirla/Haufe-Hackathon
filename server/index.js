d16c83e618084c88d6f66923bc4a9af9359296fd
unknown <alexbirla32@gmail.com>
2022-10-01 12:07:25 +0300

    Setup express server that responds with required data

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-// TODO: start here
+const express = require('express')
+const axios = require('axios')
+const { response } = require('express')
+const app = express()
+const port = 5000
+
+app.get('/', async (req, res) => {
+  await axios.get('https://data.primariatm.ro/api/3/action/datastore_search?resource_id=d0134630-84d9-40b8-9bcb-dfdc926d66ab').then((res) =>{
+    collectors=res.data.result.records
+})
+res.send(collectors)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
